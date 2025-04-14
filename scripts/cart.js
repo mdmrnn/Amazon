294756c6a537217dc72c36066f887b7aa37afb37
mohammad-nakhjavani <mdm.rnn@gmail.com>
2025-04-14 09:20:32 -0700

    save cart in local storage

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,4 +1,4 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem("cart")) || [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -39,6 +39,7 @@ export function removeFromCart(id) {
     if (cartItem.id != id) newCart.push(cartItem);
   });
   cart = newCart;
+  localStorage.setItem("cart", JSON.stringify(cart));
   //renderOrderSummaryHTML();
   //updateCartQuantity();
   console.log(cart);
