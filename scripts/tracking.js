b9897d6537e3d492fe31425742919045ef225f53
mohammad-nakhjavani <mdm.rnn@gmail.com>
2025-05-01 06:02:02 -0700

    activation of progress bar in tracking page

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -9,15 +9,25 @@ function trackingSummary() {
   const productId = url.searchParams.get("productId");
   const order = findOrder(orderId);
   const product = findProduct(productId);
-
   const orderProduct = findOrderProduct(productId, order);
-  const delivaryTime = formatTime(orderProduct.estimatedDeliveryTime);
+
+  const now = dayjs() + 2;
+  const delivaryTime = dayjs(orderProduct.estimatedDeliveryTime);
+  const orderTime = dayjs(order.orderTime);
+  const progressPercent = Math.round(
+    ((now - orderTime) / (delivaryTime - orderTime)) * 100
+  );
+  //let status;
+  //style='color: rgb(6, 125, 98);'
+
   document.querySelector(".js-order-tracking").innerHTML = `
     <a class="back-to-orders-link link-primary" href="orders.html">
       View all orders
     </a>
 
-    <div class="delivery-date">Arriving on ${delivaryTime}</div>
+    <div class="delivery-date">Arriving on ${delivaryTime.format(
+      "dddd, MMMM D"
+    )}</div>
   
     <div class="product-info">
       ${product.name}
@@ -31,15 +41,28 @@ function trackingSummary() {
     />
 
     <div class="progress-labels-container">
-      <div class="progress-label">Preparing</div>
-      <div class="progress-label current-status">Shipped</div>
-      <div class="progress-label">Delivered</div>
+      <div class="progress-label js-progress-label-preparing">Preparing</div>
+      <div class="progress-label js-progress-label-shipped">Shipped</div>
+      <div class="progress-label js-progress-label-delivered">Delivered</div>
     </div>
 
     <div class="progress-bar-container">
-      <div class="progress-bar"></div>
+      <div class="progress-bar" style='width:${progressPercent}%;'></div>
     </div>
   `;
+
+  if (progressPercent < 50)
+    document
+      .querySelector(".js-progress-label-preparing")
+      .classList.add("current-status");
+  else if (progressPercent < 100)
+    document
+      .querySelector(".js-progress-label-shipped")
+      .classList.add("current-status");
+  else
+    document
+      .querySelector(".js-progress-label-delivered")
+      .classList.add("current-status");
 }
 
 async function renderTracking() {
